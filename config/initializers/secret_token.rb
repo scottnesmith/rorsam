9c393548811b96c2440e7a377bbfa273ebd47c16
Scott Nesmith <scottnesmith@gmail.com>
2011-12-21 16:07:33 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rorsam::Application.config.secret_token = '51d5dfe9dd17b994a40084102510848001d7e1a14ff6bfb9a1934d1384f0ceaceaec05ef2d059b238ad74446c39f85e8bd80914ac1c470c7412e546d1a1708c9'
